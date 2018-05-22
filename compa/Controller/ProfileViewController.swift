8d7e3d17ecffa9a35bad58a0275fac0678d56fef
merzoukRb <merzouk94@gmail.com>
2018-05-22 18:46:02 +0200

    working on profileTableView

--- compa/Controller/ProfileViewController.swift
+++ compa/Controller/ProfileViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var login: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     
+    let list = ["Jean", "Franck", "Marc"]
+    
+    @IBOutlet weak var friendName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +29,26 @@ class ProfileViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.updateUIView()
     }
+    
+    //TableView
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ProfileTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        //TODO récupéré la liste des friends de User
+        //let friends = User.getFriends()[indexPath.row] ?
+        //friends.username; user.image
+        cell.friendName?.text = list[indexPath.row]
+        cell.friendImage?.image = #imageLiteral(resourceName: "person-profile")
+        
+        return cell
+    }
+    //Fin Table
     
     func updateUIView() {
         self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2
