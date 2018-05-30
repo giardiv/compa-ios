864b811b2a77481a6207b23340bd6ccf827c60c8
Sarah <mouffoksarah1@gmail.com>
2018-05-30 04:21:56 -0700

    Update User.swift

--- compa/Model/User.swift
+++ compa/Model/User.swift
@@ -11,36 +11,10 @@ import CoreLocation
 
 class User {
     
-<<<<<<< HEAD
-    let username : String
-    let friends : [User]
-=======
+
     let id, login, name : String
->>>>>>> 9d6a185ce5f5cad5fdc1ee2bc72f1b218c58881a
     let lastLocation : Location
-    
-    //let imageURL, firstName, lastName:String
-    //let dob:Date
-    
-<<<<<<< HEAD
-    init(username: String, location:Location, friends: [User]){
-        self.username = username
-        self.friends = friends
-        self.lastLocation = location
-    }
-    
-    convenience init?(dictionary: Dictionary<String, AnyObject>) {
-        let name = dictionary["username"]! as! String
         
-        let location = Location(dictionary: dictionary["location"]! as! Dictionary<String, AnyObject>)
-        
-        //User.getMockLocationsFor(CLLocation(latitude:51.509865, longitude:-0.118092)) // to change
-       
-        let friendsDic = dictionary["friends"]! as! Dictionary<String, AnyObject>
-        //let friends = friendsDic.map { User(dictionary: $0 as! [String:AnyObject]!) }
-        
-        //self.init(username: name, location: location, friends: friends)
-=======
     init(login: String, location:Location, name:String, id:String){
         self.login = login
         self.name = name
@@ -54,25 +28,13 @@ class User {
         let id = dictionary["id"]! as! String
         let location = Location(dictionary: dictionary["lastLocation"]! as! [String:Any])
         self.init(login:login, location: location, name:name, id:id)
->>>>>>> 9d6a185ce5f5cad5fdc1ee2bc72f1b218c58881a
     }
 
-    
-<<<<<<< HEAD
     convenience init(){
         self.init(username:"soMysterious", location: Location(), friends: [User]())
     }
     
- 
-    static func getMockLocationsFor(_ location: CLLocation) -> Dictionary<Date, CLLocation> {
-=======
-    
-    //User.getMockLocationsFor(CLLocation(latitude:51.509865, longitude:-0.118092)) // to change
-    
-    
-    static func getMockLocationsFor(_ location: CLLocation) -> [Date:CLLocation] {
->>>>>>> 9d6a185ce5f5cad5fdc1ee2bc72f1b218c58881a
-        
+    static func getMockLocationsFor(_ location: CLLocation) -> [Date:CLLocation] {    
         func getBase(number: Double) -> Double {
             return round(number * 1000)/1000
         }
