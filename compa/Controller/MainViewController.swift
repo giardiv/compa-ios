0ae4cc2961310cedae2bf25ab8c0d60dec3660a1
Sarah <mouffoksarah1@gmail.com>
2018-06-04 20:54:39 +0200

    refactor segues

--- compa/Controller/MainViewController.swift
+++ compa/Controller/MainViewController.swift
@@ -1,29 +1,9 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  compa
 //
-//  Created by m2sar on 12/04/2018.
+//  Created by m2sar on 04/06/2018.
 //  Copyright © 2018 m2sar. All rights reserved.
 //
 
-import UIKit
-
-class MainViewController: UIViewController {
-    
-    override func viewWillAppear(_ animated: Bool) {
-        let mapView = self.storyboard?.instantiateViewController(withIdentifier: "mapView") as! MapViewController
-        let token = UserDefaults.standard.value(forKey: "token")
-        if(token != nil){
-            self.present(mapView, animated: true)
-        }
-    }
-    
-    @IBAction func loginButtonTapped(_ sender: UIButton) {
-        self.performSegue(withIdentifier:"mainToLogin", sender: self)
-    }
-
-    @IBAction func registerButtonTapped(_ sender: UIButton) {
-        self.performSegue(withIdentifier:"mainToRegister", sender: self)
-    }
-}
-
+import Foundation
