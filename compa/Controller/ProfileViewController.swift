87be67cadb58923ca43d32adeec2e234f89158af
merzoukRb <merzouk94@gmail.com>
2018-06-06 16:05:58 +0200

    refonte de la view profile : ajout d'une map dans la view profile

--- compa/Controller/ProfileViewController.swift
+++ compa/Controller/ProfileViewController.swift
@@ -2,206 +2,50 @@
 //  ProfileViewController.swift
 //  compa
 //
-//  Created by m2sar on 18/05/2018.
+//  Created by m2sar on 06/06/2018.
 //  Copyright © 2018 m2sar. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ProfileViewController: UIViewController, MKMapViewDelegate {
     
-    @IBOutlet weak var login: UILabel!
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userProfileName: UILabel!
+    @IBOutlet weak var userProfileImage: UIImageView!
     
-    @IBOutlet weak var table: UITableView!
-    @IBOutlet weak var search: SearchTextField!
+    @IBOutlet weak var userProfileMap: MKMapView!
     
-    let repo = UserRepository()
-    let friendshipRep = FriendshipRepository()
-    var selectedUser : User?
-    
-    var userArray : [User] = []
-    
-    
-    @IBOutlet weak var addUserButton: UIBarButtonItem!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-        
-        search.theme.bgColor = UIColor.white.withAlphaComponent(1)
-        search.theme.borderColor = UIColor.black.withAlphaComponent(0.5)
-        
-        search.userStoppedTypingHandler = {
-            if let criteria = self.search.text {
-                if criteria.characters.count > 1 {
-                    
-                    self.search.showLoadingIndicator()
-                    
-                    self.repo.search(
-                        text: criteria,
-                        result: { data in
-                            
-                            let results = data.map { SearchTextFieldItem(title: $0.name, subtitle: $0.login, image: nil, user:$0) }
-                            
-                            DispatchQueue.main.async {
-                                self.search.filterItems(results)
-                                self.search.stopLoadingIndicator()
-                            }
-                        },
-                        
-                        error:{ error in
-                            
-                            DispatchQueue.main.async {
-                                self.search.filterItems([])
-                            }
-                        }
-                    )
-                    
-                }
-            }
-        }
-        
-        search.itemSelectionHandler = { data, index in
-             self.selectedUser = data[index].user!
-             self.addUserButton?.isEnabled = true
-        }
-        
-        search.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-        
-    }
-    
-    func textFieldDidChange(_ textField: UITextField) {
-        selectedUser = nil
-        addUserButton?.isEnabled = false
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    let userRepositoty = UserRepository()
     
     override func viewWillAppear(_ animated: Bool) {
-        
         let sv = UIViewController.displaySpinner(onView: self.view)
-        self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2
-        
+        self.userProfileImage.layer.cornerRadius = self.userProfileImage.frame.size.width / 2
+
         let ctrl = self
-        
-        repo.getAuthUser(
+
+        userRepositoty.getAuthUser(
             result: { user in
                 DispatchQueue.main.async(execute: {
-                    ctrl.profileImage.image = #imageLiteral(resourceName: "images") //TODO
-                    ctrl.login.text = user.name
+                    ctrl.userProfileImage.image = #imageLiteral(resourceName: "images") //TODO
+                    ctrl.userProfileName.text = user.name
                     UIViewController.removeSpinner(spinner: sv)
                 })
-                
+
             },
-            
+
             error: {error in
-                if( self.checkToken(error: error, spinner:sv) ) {
-                    
-                    DispatchQueue.main.async(execute: {
-                        UIViewController.removeSpinner(spinner: sv)
-                        self.alert(error["message"] as! String)
-                    })
-                    
+                if( self.checkToken(error: error, spinner:sv)) {
+                    UIViewController.removeSpinner(spinner: sv)
+                    self.alert(error["message"] as! String)
                 }
             }
         )
         
-      reloadTable()
-        
     }
     
-    
-    func reloadTable(){
-        
-        let sv = UIViewController.displaySpinner(onView: self.view)
-        
-        repo.getFriends (
-            result: { data in
-                self.userArray = data
-                
-                DispatchQueue.main.async(execute: {
-                    self.table.reloadData()
-                    UIViewController.removeSpinner(spinner: sv)
-                })
-                
-            },
-            error: {error in
-                
-                if( self.checkToken(error: error, spinner:sv) ) {
-                    
-                    DispatchQueue.main.async(execute: {
-                        UIViewController.removeSpinner(spinner: sv)
-                        self.alert(error["message"] as! String)
-                    })
-                    
-                }
-                
-            }
-        )
-    }
-    
-    //TableView
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return userArray.count
-    }
-    
-    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Friends"
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as? FriendCell  else {
-            fatalError()
-        }
-        cell.cellName?.text = userArray[indexPath.row].name
-        cell.cellImage?.image = #imageLiteral(resourceName: "person-profile") //TODO
-        
-        return cell
-    }
-    
-    
-    @IBAction func searchButtonTapped(_ sender: Any) {
-       
-        if let user = selectedUser {
-            friendshipRep.requestFriendship (
-                friendId: user.id,
-                result: { data in
-                    DispatchQueue.main.async {
-                        self.reloadTable()
-                    }
-                },
-                error: { error in
-                    DispatchQueue.main.async {
-                        self.alert(error["message"] as! String)
-                    }
-                }
-            )
-        }
-        
-    }
-   
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedFriend = userArray[indexPath.row]
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "FriendProfile") as! FriendProfileViewController
-        vc.friendId = selectedFriend.id
-        vc.status = "Accepted"
-        self.present(vc, animated: true, completion: nil)
-    }
-    
-    //Fin Table
-    
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
-    }
-    
-   
-    @IBAction func mapButtonTapped(_ sender: UIBarButtonItem) {
+    @IBAction func profileToMapTapped(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "profileToMap", sender: self)
     }
- 
+    
 }
